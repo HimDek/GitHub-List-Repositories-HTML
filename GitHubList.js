76542400f3c11cbb112c51b25da95224f54fed22
Himprakash Deka <himprakashd@gmail.com>
2023-02-20 11:33:11 +0530

    Update GitHubList.js

--- GitHubList.js
+++ GitHubList.js
@@ -79,6 +79,7 @@ function gitpin(apiurl, type, element) {
         url = out.homepage;
 
       forkspan = document.createElement("span");
+      forkspan.classList.add("d-none");
       if (out.forks >= 1) {
         forkurl = "https://github.com/" + out.owner.login.toLowerCase() + "/" + out.name.toLowerCase() + "/network/members"
         forksvg = "<svg aria-hidden=\"true\" height=\"19\" viewBox=\"0 0 16 16\" version=\"1.1\" width=\"16\" data-view-component=\"true\"><path fill-rule=\"evenodd\" d=\"M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z\"></path></svg>"
@@ -87,10 +88,12 @@ function gitpin(apiurl, type, element) {
         forka.setAttribute("href", forkurl);
         forka.innerHTML = forksvg + out.forks.toString();
         
+        forkspan.classList.remove("d-none");
         forkspan.appendChild(forka);
       }
 
       starspan = document.createElement("span");
+      starspan.classList.add("d-none");
       if (out.stargazers_count >= 1) {
         starurl = "https://github.com/" + out.owner.login.toLowerCase() + "/" + out.name.toLowerCase() + "/stargazers";
         starsvg = "<svg aria-hidden=\"true\" height=\"19\" viewBox=\"0 0 16 16\" version=\"1.1\" width=\"16\" data-view-component=\"true\"><path fill-rule=\"evenodd\" d=\"M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z\"></path></svg>"
@@ -99,19 +102,26 @@ function gitpin(apiurl, type, element) {
         stara.setAttribute("href", starurl);
         stara.innerHTML = starsvg + out.stargazers_count.toString();
         
+        starspan.classList.remove("d-none");
         starspan.appendChild(stara);
       }
       
       languagespan = document.createElement("span");
+      languagespan.classList.add("d-none");
       if (out.language != null) {
         colorize(out.id, out.language);
         languagesvg = "<svg style=\"padding: 0px; margin:0;\" height=\"16\" width=\"14\"><circle id=\"" + out.id + "\" cx=\"6\" cy=\"6\" r=\"6\" stroke=\"white\" stroke-width=\"1\"></circle></svg>"
+        
+        languagespan.classList.remove("d-none");
         languagespan.innerHTML = languagesvg + out.language;
       }
 
       licensespan = document.createElement("span");
+      licensespan.classList.add("d-none");
       if (out.license != null) {
         licensesvg = "<svg aria-hidden=\"true\" height=\"19\" viewBox=\"0 0 16 16\" version=\"1.1\" width=\"19\" data-view-component=\"true\" class=\"octicon octicon-law mr-1\"><path fill-rule=\"evenodd\" d=\"M8.75.75a.75.75 0 00-1.5 0V2h-.984c-.305 0-.604.08-.869.23l-1.288.737A.25.25 0 013.984 3H1.75a.75.75 0 000 1.5h.428L.066 9.192a.75.75 0 00.154.838l.53-.53-.53.53v.001l.002.002.002.002.006.006.016.015.045.04a3.514 3.514 0 00.686.45A4.492 4.492 0 003 11c.88 0 1.556-.22 2.023-.454a3.515 3.515 0 00.686-.45l.045-.04.016-.015.006-.006.002-.002.001-.002L5.25 9.5l.53.53a.75.75 0 00.154-.838L3.822 4.5h.162c.305 0 .604-.08.869-.23l1.289-.737a.25.25 0 01.124-.033h.984V13h-2.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-2.5V3.5h.984a.25.25 0 01.124.033l1.29.736c.264.152.563.231.868.231h.162l-2.112 4.692a.75.75 0 00.154.838l.53-.53-.53.53v.001l.002.002.002.002.006.006.016.015.045.04a3.517 3.517 0 00.686.45A4.492 4.492 0 0013 11c.88 0 1.556-.22 2.023-.454a3.512 3.512 0 00.686-.45l.045-.04.01-.01.006-.005.006-.006.002-.002.001-.002-.529-.531.53.53a.75.75 0 00.154-.838L13.823 4.5h.427a.75.75 0 000-1.5h-2.234a.25.25 0 01-.124-.033l-1.29-.736A1.75 1.75 0 009.735 2H8.75V.75zM1.695 9.227c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L3 6.327l-1.305 2.9zm10 0c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L13 6.327l-1.305 2.9z\"></path></svg>";
+        
+        licensespan.classList.remove("d-none");
         licensespan.innerHTML = licensesvg  + out.license.name;
       }
       
@@ -140,53 +150,100 @@ function gitpin(apiurl, type, element) {
     }
 
     if (type == "gist") {
-      files = 0;
+      
+      filespan = document.createElement("span");
+      filespan.classList.add("d-none");
+      
+      filecount = 0;
     	for (let i = 0; ; i++) {
       	if (Object.keys(out.files)[i] == null)
         	break;
-        files++;
+        filecount++;
       }
-      if (files == 0)
-        files = "";
-      else { 
-        if (files == 1)
-      	  files = files + " file";
+      
+      if (filecount != 0) {
+        if (filecount == 1)
+      	  filetxt = filecount + " file";
         else
-      	  files = files + " files";
-        files = "<span><a href=\"" + out.html_url + "\"><svg aria-hidden=\"true\" height=\"19\" viewBox=\"0 0 16 16\" version=\"1.1\" width=\"16\" data-view-component=\"true\" class=\"octicon octicon-code-square\"><path fill-rule=\"evenodd\" d=\"M1.75 1.5a.25.25 0 00-.25.25v12.5c0 .138.112.25.25.25h12.5a.25.25 0 00.25-.25V1.75a.25.25 0 00-.25-.25H1.75zM0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v12.5A1.75 1.75 0 0114.25 16H1.75A1.75 1.75 0 010 14.25V1.75zm9.22 3.72a.75.75 0 000 1.06L10.69 8 9.22 9.47a.75.75 0 101.06 1.06l2-2a.75.75 0 000-1.06l-2-2a.75.75 0 00-1.06 0zM6.78 6.53a.75.75 0 00-1.06-1.06l-2 2a.75.75 0 000 1.06l2 2a.75.75 0 101.06-1.06L5.31 8l1.47-1.47z\"></path></svg>" + files + "</a></span>";
+      	  filetxt = filecount + " files";
+      
+        filesvg = "<svg aria-hidden=\"true\" height=\"19\" viewBox=\"0 0 16 16\" version=\"1.1\" width=\"16\" data-view-component=\"true\" class=\"octicon octicon-code-square\"><path fill-rule=\"evenodd\" d=\"M1.75 1.5a.25.25 0 00-.25.25v12.5c0 .138.112.25.25.25h12.5a.25.25 0 00.25-.25V1.75a.25.25 0 00-.25-.25H1.75zM0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v12.5A1.75 1.75 0 0114.25 16H1.75A1.75 1.75 0 010 14.25V1.75zm9.22 3.72a.75.75 0 000 1.06L10.69 8 9.22 9.47a.75.75 0 101.06 1.06l2-2a.75.75 0 000-1.06l-2-2a.75.75 0 00-1.06 0zM6.78 6.53a.75.75 0 00-1.06-1.06l-2 2a.75.75 0 000 1.06l2 2a.75.75 0 101.06-1.06L5.31 8l1.47-1.47z\"></path></svg>";
+      
+        filea = document.createElement("a");
+        filea.setAttribute("href", out.html_url);
+        filea.innerHTML = filesvg + filetxt;
+      
+        filespan.classList.remove("d-none");
+        filespan.appendChild(filea);
       }
 
-      forks = 0;
+      forkspan = document.createElement("span");
+      forkspan.classList.add("d-none");
+      
+      forkcount = 0;
     	for (let i = 0; ; i++) {
       	if (Object.keys(out.forks)[i] == null)
         	break;
-        forks++;
+        forkcount++;
       }
-      if (forks == 0)
-        forks = "";
-      else {
-        if (forks == 1)
-      	  forks = forks + " fork";
+      if (forkcount != 0) {
+        if (forkcount == 1)
+      	  forktxt = forkcount + " fork";
         else
-      	  forks = forks + " forks";
-        forks = "<span><a href=\"" + out.html_url + "/forks\"><svg aria-hidden=\"true\" height=\"19\" viewBox=\"0 0 16 16\" version=\"1.1\" width=\"16\" data-view-component=\"true\"><path fill-rule=\"evenodd\" d=\"M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z\"></path></svg>" + forks + "</a></span>";
+      	  forktxt = forkcount + " forks";
+        
+        forksvg = "<svg aria-hidden=\"true\" height=\"19\" viewBox=\"0 0 16 16\" version=\"1.1\" width=\"16\" data-view-component=\"true\"><path fill-rule=\"evenodd\" d=\"M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z\"></path></svg>";
+        
+        forka = document.createElement("a");
+        forka.setAttribute("href", out.html_url + "/forks");
+        forka.innerHTML = forksvg + forktxt;
+        
+        forkspan.classList.remove("d-none");
+        forkspan.appendChild(forka);
       }
 
-      if (out.comments == 0)
-        comments = ""
-      else {
-        if (out.comments == 1)
-      	  comments = out.comments + " comment";
-        else
-      	  comments = out.comments + " comments";
-        comments = "<span><a href=\"" + out.html_url + "#comments\"><svg aria-hidden=\"true\" height=\"19\" viewBox=\"0 0 16 16\" version=\"1.1\" width=\"16\" data-view-component=\"true\" class=\"octicon octicon-comment\"><path fill-rule=\"evenodd\" d=\"M2.75 2.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h2a.75.75 0 01.75.75v2.19l2.72-2.72a.75.75 0 01.53-.22h4.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25H2.75zM1 2.75C1 1.784 1.784 1 2.75 1h10.5c.966 0 1.75.784 1.75 1.75v7.5A1.75 1.75 0 0113.25 12H9.06l-2.573 2.573A1.457 1.457 0 014 13.543V12H2.75A1.75 1.75 0 011 10.25v-7.5z\"></path></svg>" + comments + "</a></span>"
-      }
-
-      stats = "<span class=\"stats\">" + files + forks + comments + "</span>";
-
-      divtxt = "<a href = \"" + url + "\"><h2>" + Object.keys(out.files)[0] + "</h2></a><p>" + desc + "</p>" + stats;
+      commentspan = document.createElement("span");
+      commentspan.classList.add("d-none");
       
-      card.innerHTML = divtxt;
+      if (out.comments != 0) {
+        if (out.comments == 1)
+      	  commenttxt = out.comments + " comment";
+        else
+      	  commenttxt = out.comments + " comments";
+        
+        commentsvg = "<svg aria-hidden=\"true\" height=\"19\" viewBox=\"0 0 16 16\" version=\"1.1\" width=\"16\" data-view-component=\"true\" class=\"octicon octicon-comment\"><path fill-rule=\"evenodd\" d=\"M2.75 2.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h2a.75.75 0 01.75.75v2.19l2.72-2.72a.75.75 0 01.53-.22h4.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25H2.75zM1 2.75C1 1.784 1.784 1 2.75 1h10.5c.966 0 1.75.784 1.75 1.75v7.5A1.75 1.75 0 0113.25 12H9.06l-2.573 2.573A1.457 1.457 0 014 13.543V12H2.75A1.75 1.75 0 011 10.25v-7.5z\"></path></svg>";
+        
+        commenta = document.createElement("a");
+        commenta.setAttribute("href", out.html_url + "#comments");
+        commenta.innerHTML = commentsvg + commenttxt;
+        
+        commentspan.classList.remove("d-none");
+        commentspan.appendChild(commenta);
+      }
+
+      statsspan = document.createElement("span");
+      statsspan.setAttribute("class", "stats");
+      
+      statsspan.appendChild(filespan);
+      statsspan.appendChild(forkspan);
+      statsspan.appendChild(commentspan);
+      
+      heading = document.createElement("h2");
+      heading.innerHTML = Object.keys(out.files)[0];
+      
+      cardtitle = document.createElement("a");
+      cardtitle.setAttribute("href", url);
+      cardtitle.appendChild(heading);
+      
+      cardcontent = document.createElement("p");
+      cardcontent.innerHTML = desc;
+      
+      cardfooter = statsspan;
+      
+      card.classList.add("gist");
+      card.appendChild(cardtitle);
+      card.appendChild(cardcontent);
+      card.appendChild(cardfooter);
     }
     element.appendChild(card);
   });
